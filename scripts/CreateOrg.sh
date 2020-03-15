fe49e0051dac4f74ba24e284c20a8b4e09a11ff1
Don Robins <don@forcementor.com>
2020-03-14 18:20:39 -0700

    Project Initialization

---
+++ scripts/CreateOrg.sh
@@ -0,0 +1,10 @@
+# Execute in Mac using: ./scripts/CreateOrg.sh
+#sfdx force:auth:web:login -d -a dhDBRDev
+echo "*** Creating scratch Org..."
+#sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soLwcDnd -d 30
+echo "*** Opening scratch Org..."
+sfdx force:org:open
+echo "*** Pushing metadata to scratch Org..."
+sfdx force:source:push
+echo "*** Push data to scratch Org..."
+sfdx force:apex:execute -f ./scripts/ResetData.txt
