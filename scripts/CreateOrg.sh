84e42d181ac736c122071e751d9934a6e94a2e89
Don Robins <don@forcementor.com>
2020-05-12 14:33:38 -0700

    Move open org to script end

--- scripts/CreateOrg.sh
+++ scripts/CreateOrg.sh
@@ -1,9 +1,9 @@
 # Execute in Mac using: ./scripts/CreateOrg.sh
 echo "*** Creating scratch Org..."
 sfdx force:org:create -f config/project-scratch-def.json --setdefaultusername --setalias soDnD -d 30
-echo "*** Opening scratch Org..."
-sfdx force:org:open
 echo "*** Pushing metadata to scratch Org..."
 sfdx force:source:push
 echo "*** Push data to scratch Org..."
 sfdx force:apex:execute -f ./scripts/ResetData.txt
+echo "*** Opening scratch Org..."
+sfdx force:org:open
